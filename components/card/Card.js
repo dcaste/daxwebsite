2cc7ececb61d4f794574e50a1078dab1b86a1008
unknown <daxcastellon@gmail.com>
2020-10-13 15:00:10 +0200

    Fixed Card and global contentSpacing styles

--- components/card/Card.js
+++ components/card/Card.js
@@ -1,7 +1,8 @@
 import styles from './Card.module.scss';
+import classNames from 'classnames';
 
-const Card = (props) => {
-	return <div className={styles.card}>{props.children}</div>;
+const Card = ({ children, className }) => {
+	return <div className={classNames(className, styles.card)}>{children}</div>;
 };
 
 export default Card;
