a25ae2e45e3ac8d58b3b350468e5e44472d4e3ef
dcaste <dax@daxcastellon.dev>
2020-12-29 21:21:35 +0100

    Fixed copyright and Footer heights

--- components/copyright/CopyRight.js
+++ components/copyright/CopyRight.js
@@ -3,8 +3,8 @@ import styles from './CopyRight.module.scss';
 const CopyRight = () => {
 	return (
 		<p className={styles.copyRight}>
-			© {new Date().getFullYear()}, Built with{' '}
-			<a href='https://nextjs.org/'>NextJS</a>
+			© {new Date().getFullYear()}, Dax Castellón Meyrat <br />
+			Built with <a href='https://nextjs.org/'>NextJS</a>
 		</p>
 	);
 };
