b8b599e9e27eb282d060247fee5866c2409fe8fc
dcaste <dax@daxcastellon.dev>
2021-03-08 15:16:54 +0100

    Added content to Dax Website page

--- pages/dax-website.js
+++ pages/dax-website.js
@@ -23,16 +23,11 @@ const CampbellsKitchen = () => {
 			<h1 className='pageTitle'>Dax Castellón Website</h1>
 
 			<Image
-				src='/assets/img/campbells-kitchen.jpg'
-				alt='Dax Castellón Website'
+				src='/assets/img/daxwebsite-intro.jpg'
+				alt='Dax Castellón Website intro'
 				width={1400}
-				height={875}
+				height={650}
 				className='bordered'
-			/>
-			<VisitWebsite
-				URL='https://campbells.com/kitchen/'
-				Tag='p'
-				className='imageDesc'
 			/>
 			<p className='imageDesc'>
 				<a target='_blank' rel='external' href='https://github.com/dcaste/'>
@@ -51,21 +46,18 @@ const CampbellsKitchen = () => {
 							company by showing my skills as web developer.
 						</p>
 						<p className='lead'>
-							This website is built with a clean design to ease finding
-							information and loads extremely fast by using new Web
+							This website is built with a clean design so users can find
+							information easily and loads extremely fast by using new Web
 							technologies.
 						</p>
 					</div>
 					<div className={styles.content}>
 						<p>
-							<strong>Position:</strong> Freelance web developer
-						</p>
-						<p>
 							<strong>Technologies:</strong>
 						</p>
 						<ul>
-							<li>NextJS framework</li>
-							<li>HTML5, SASS, CSS3, Javascript ES6</li>
+							<li>NextJs framework</li>
+							<li>HTML5, SASS, CSS3, React</li>
 							<li>Cloud hosting</li>
 							<li>EmailJS as email service</li>
 						</ul>
@@ -79,10 +71,13 @@ const CampbellsKitchen = () => {
 				<h2 className='textCentered'>The Concept</h2>
 				<p>
 					I believe that every project should start by defining its purpose, who
-					we are and to whom we are speaking to. This will guide every decision
-					to be made, from the language to be used, the look and feel and even
-					the technologies. If these definitions are not made, the project will
-					not a have a clear goal and indentity.
+					you are and to whom you are talking to. If these definitions are not
+					made, the project will not a have a clear goal and indentity.
+				</p>
+				<h3 className='textCentered'>The Purpose</h3>
+				<p>
+					The goal of this website is to show my skills as Web Developer and
+					serve as portfolio to help me get a position in a company.
 				</p>
 			</div>
 
@@ -144,8 +139,8 @@ const CampbellsKitchen = () => {
 					<p>
 						I think the look should be a little casual because I want people to
 						feel that they are meeting a real person, a future team member who
-						they can relate and not another boring candidate. Hence the use of
-						emoticons and casual photos of me.
+						they can relate to and not another boring candidate. Hence the use
+						of emoticons and casual photos of me.
 					</p>
 					<p>
 						Also the color choices are not random. I chose red because evokes
@@ -159,10 +154,10 @@ const CampbellsKitchen = () => {
 				</div>
 				<div className={styles.picture}>
 					<Image
-						src='/assets/img/campbells-kitchen-block.jpg'
-						alt='Dax Castellón block of content'
-						width={1439}
-						height={513}
+						src='/assets/img/daxwebsite-look.jpg'
+						alt='Dax Castellón Website Look and Feel'
+						width={1400}
+						height={1037}
 						className='bordered'
 					/>
 				</div>
@@ -175,8 +170,8 @@ const CampbellsKitchen = () => {
 					<h3>The Language</h3>
 					<p>
 						My language is casual, the same I would use in a work environment. I
-						want to be understood quick and I would like people to relate. I use
-						some technical jargon but only when is needed.
+						want to be understood quickly and I would like people to relate. I
+						use some technical jargon but only when is needed.
 					</p>
 					<p>
 						Also I speak in first person because I am presenting my story, my
@@ -186,10 +181,10 @@ const CampbellsKitchen = () => {
 				</div>
 				<div className={styles.picture}>
 					<Image
-						src='/assets/img/campbells-kitchen-reviews.jpg'
-						alt='Dax Castellón Reviews feature'
-						width={640}
-						height={640}
+						src='/assets/img/daxwebsite-language.jpg'
+						alt='Dax Castellón website, language and structure'
+						width={1400}
+						height={916}
 						className='bordered'
 					/>
 				</div>
@@ -209,6 +204,11 @@ const CampbellsKitchen = () => {
 					, so that way I could have a decoupled Backend from Frontend. The
 					other reason for this is great performance and low costs.
 				</p>
+				<p>
+					At this moment, this website is a static HTML built with NextJs, but
+					my idea in the future is to connect it to a CMS, so JAMSTACK seemed
+					like the best decision.
+				</p>
 			</div>
 
 			<Spacer height='double' />
@@ -221,11 +221,10 @@ const CampbellsKitchen = () => {
 						<a href='https://reactjs.org/' target='_blank' rel='external'>
 							ReactJs
 						</a>{' '}
-						since the development process is very enjoyable, the code is easy to
-						maintain and there are a large amount of libraries that I could use.
-						The downside is that a React app is not readable by web crawlers
-						since the HTML is not rendered until Javascript is executed, that is
-						the reason I chose NextJS.
+						since the development process is very enjoyable and the code is easy
+						to maintain. The downside is that a React app is not readable by web
+						crawlers since the HTML is not rendered until Javascript is
+						executed, that is the reason I chose NextJs.
 					</p>
 					<h4>SEO</h4>
 					<p>
@@ -233,34 +232,16 @@ const CampbellsKitchen = () => {
 							NextJs
 						</a>{' '}
 						is a React framework that prerenders the HTML so web crawlers can
-						read what the webpage is about and that is needed for Search Engine
-						Optimization.
-					</p>
-					<h4>Speed</h4>
-					<p>
-						Another consideration was that NextJs can export the final result in
-						static HTML files and the final product renders super fast. Also,
-						these small static files can be uploaded to static cloud hostings
-						for blazing performance.
-					</p>
-					<h4>Better developer experience</h4>
-					<p>
-						Next also gives among other advantages: image optmization,
-						incremental static generation, an easy file system rounting, code
-						splitting and bundling, built in CSS support and all of this with
-						almost zero configuration.
-					</p>
-					<p>
-						I tried GatsbyJs as well, but it was slow and it gave me a lot
-						compatibility issues between node modules.{' '}
+						read what the webpage is about which is needed for indexing the
+						website and show it in search results.
 					</p>
 				</div>
 				<div className={styles.picture}>
 					<Image
-						src='/assets/img/campbells-kitchen-block.jpg'
+						src='/assets/img/daxwebsite-technologies.jpg'
 						alt='Dax Castellón block of content'
-						width={1439}
-						height={513}
+						width={1280}
+						height={800}
 						className='bordered'
 					/>
 				</div>
@@ -269,6 +250,46 @@ const CampbellsKitchen = () => {
 			<Spacer height='double' />
 
 			<SplitContent split='50-50' direction='rl'>
+				<div className={styles.content}>
+					<h4>Easy Deployments</h4>
+					<p>
+						All of the deployments are made through GIT. I can work in separate
+						branches or work in new features and once they are finished I just
+						upload them with GIT, merge the changes to the Main branch which is
+						then automatically uploaded to the production server.
+					</p>
+					<h4>Speed</h4>
+					<p>
+						NextJs can export the final result in static HTML files and the
+						final product renders super fast. Also, these small static files can
+						be uploaded to static cloud hostings for blazing performance.
+					</p>
+					<p>
+						The source code is uploaded to a GIT repository, but only the final
+						static HTML is uploaded to the production server.
+					</p>
+					<h4>Better developer experience</h4>
+					<p>
+						Next also gives among other advantages: image optmization,
+						incremental static generation, an easy file system rounting, code
+						splitting and bundling, built in CSS support and all of this with
+						almost zero configuration.
+					</p>
+				</div>
+				<div className={styles.picture}>
+					<Image
+						src='/assets/img/daxwebsite-git.jpg'
+						alt='Dax Castellón website deployments via GIT'
+						width={1400}
+						height={1095}
+						className='bordered'
+					/>
+				</div>
+			</SplitContent>
+
+			<Spacer height='double' />
+
+			<SplitContent split='50-50'>
 				<div className={styles.content}>
 					<h3>EmailJS</h3>
 					<p>
@@ -289,16 +310,16 @@ const CampbellsKitchen = () => {
 						>
 							Nodemailer
 						</a>{' '}
-						but because of the small needs for this project, EmailJS is the
-						fastest and easiest option in my opinion.
+						but since this website has small needs, EmailJS is the fastest and
+						easiest option to implement in my opinion.
 					</p>
 				</div>
 				<div className={styles.picture}>
 					<Image
-						src='/assets/img/campbells-kitchen-reviews.jpg'
-						alt='Dax Castellón Reviews feature'
-						width={640}
-						height={640}
+						src='/assets/img/emailjs.jpg'
+						alt='EmailJS email services'
+						width={1400}
+						height={875}
 						className='bordered'
 					/>
 				</div>
@@ -306,14 +327,14 @@ const CampbellsKitchen = () => {
 
 			<Spacer height='double' />
 
-			<SplitContent split='50-50'>
+			<SplitContent split='50-50' direction='rl'>
 				<div className={styles.content}>
 					<h2>Performance</h2>
 					<p>
 						I really enjoy the performance results. Granted, it is a static HTML
 						website but if you don't know what you are doing you can end up with
-						a very slow website even a static one. Some of the best performance
-						statistics about this website:
+						a very slow website even with a static one. Some of the best
+						performance statistics about this website:
 					</p>
 					<ul>
 						<li>The Homepage fully loads in 1 second</li>
@@ -348,10 +369,10 @@ const CampbellsKitchen = () => {
 				</div>
 				<div className={styles.picture}>
 					<Image
-						src='/assets/img/campbells-kitchen-block.jpg'
-						alt='Dax Castellón block of content'
-						width={1439}
-						height={513}
+						src='/assets/img/gtmetrix.jpg'
+						alt='Dax Castellón website performance'
+						width={1400}
+						height={875}
 						className='bordered'
 					/>
 				</div>
@@ -366,7 +387,7 @@ const CampbellsKitchen = () => {
 			<Spacer />
 
 			<Image
-				src='/assets/img/bwlm-responsive.png'
+				src='/assets/img/daxwebsite-responsive.png'
 				alt='Responsive design'
 				width={1400}
 				height={701}
@@ -385,10 +406,17 @@ const CampbellsKitchen = () => {
 						Cleaning of the code by refactoring or adding more resusable
 						components
 					</li>
-					<li>Optimizing more the code for search engines</li>
 					<li>
-						Integration of a CMS like WordPress to get more experience, althouth
-						STRAPI seems like a nice alternative
+						I am not a huge fan of using lots of animations in a website but I
+						think I should add a couple in some elements, like images
+					</li>
+					<li>
+						Optimizing more the code for search engines, like add structured
+						data and a XML mapsite
+					</li>
+					<li>
+						Integration of a CMS like WordPress, althouth STRAPI seems like a
+						better alternative
 					</li>
 				</ul>
 			</div>
