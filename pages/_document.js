a28ba390abff0d0e1812f3c5350a5a65158f6267
dcaste <dax@daxcastellon.dev>
2021-03-27 15:31:45 +0100

    Fixed incorrect Component call of HeadSEO

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import HeadSEO from '../seo/HeadSEO';
+import HeadSEO from '../components/seo/HeadSEO';
 
 export default class MyDocument extends Document {
 	render() {
