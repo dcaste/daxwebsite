26ed7a4305ec63f92e4e663965a826dac13a5444
dcaste <dax@daxcastellon.dev>
2021-04-10 22:49:29 +0200

    Added SocialLinks component

---
+++ components/sociallinks/SocialLinks.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { renderIcon, getItemKey } from './../../utils/misc';
+import styles from './SocialLinks.module.scss';
+
+const SocialLinks = ({ props }) => {
+	return (
+		<div className={styles.SocialLinks}>
+			{props.map((item) => (
+				<a
+					className={styles.item}
+					href={item.URL}
+					target='_blank'
+					key={getItemKey(item.SocialMedia, item.id)}
+				>
+					{item.SocialMedia && (
+						<span>
+							<FontAwesomeIcon icon={renderIcon(item.SocialMedia)} />
+						</span>
+					)}
+				</a>
+			))}
+		</div>
+	);
+};
+
+SocialLinks.propTypes = {
+	URL: PropTypes.string,
+	SocialMedia: PropTypes.string,
+};
+
+export default SocialLinks;
