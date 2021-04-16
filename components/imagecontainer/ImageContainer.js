e514b8b825781a89ce0aa785bbefa430310daed0
dcaste <dax@daxcastellon.dev>
2021-04-16 22:13:11 +0200

    Enhanced Image Container component

--- components/imagecontainer/ImageContainer.js
+++ components/imagecontainer/ImageContainer.js
@@ -1,16 +1,17 @@
 import Image from 'next/image';
+import styles from './ImageContainer.module.scss';
 
 function ImageWrapper({ props }) {
 	return (
-		<>
+		<div className={styles[props.style]}>
 			<Image
 				src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${props.image.url}`}
 				alt={props.image.alternativetext}
 				width={props.image.width}
 				height={props.image.height}
-				className={props.style}
+				className={props.className}
 			/>
-		</>
+		</div>
 	);
 }
 
