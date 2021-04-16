172d438821f7827ecee8799926ad4d809c8d602a
dcaste <dax@daxcastellon.dev>
2021-04-16 21:20:04 +0200

    Added alignment property to Centered Content

--- components/centeredcontent/CenteredContent.js
+++ components/centeredcontent/CenteredContent.js
@@ -4,9 +4,12 @@ import classNames from 'classnames';
 import Title from '@/title/Title';
 import styles from './CenteredContent.module.scss';
 
-const CenteredContent = ({ props: { content, title, style } }) => {
+const CenteredContent = ({ props: { content, title, alignment, style } }) => {
 	return (
-		<div className={classNames(style, styles.CenteredContent)}>
+		<div
+			className={classNames(style, styles.CenteredContent)}
+			style={{ textAlign: alignment }}
+		>
 			{title && <Title props={title} />}
 			<ReactMarkdown source={content} className={styles.Content} />
 		</div>
@@ -17,10 +20,12 @@ CenteredContent.propTypes = {
 	content: PropTypes.string.isRequired,
 	title: PropTypes.object,
 	style: PropTypes.string,
+	alignment: PropTypes.string,
 };
 
 CenteredContent.defaultProps = {
 	content: 'Please add missing content',
+	alignment: 'center',
 };
 
 export default CenteredContent;
