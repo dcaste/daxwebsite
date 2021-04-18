ef666e1dc2a8112ea0e45f6e1e63c8b8c58366db
dcaste <dax@daxcastellon.dev>
2021-04-18 20:42:49 +0200

    Fixed mobile styles and content order

--- components/projects/Projects.js
+++ components/projects/Projects.js
@@ -5,97 +5,57 @@ import ProperLink from '@/properlink/ProperLink';
 import styles from './Projects.module.scss';
 
 function Projects({ projects }) {
+	const columnOrder = (order) => {
+		const oddOrEven = order % 2 !== 0 ? 'odd' : 'even';
+		return oddOrEven;
+	};
+
 	return (
 		<>
-			{projects.map((project) =>
-				project.order % 2 !== 0 ? (
-					<div className={styles.odd} key={project.order}>
-						<div className={styles.image}>
-							<ImageContainer props={{ image: project.thumbnail }} />
-						</div>
-						<div>
-							<h2>{project.title}</h2>
-							<ReactMarkdown source={project.projectExcerpt} />
-							{project.showProjectLink && (
-								<ProperLink
-									props={{
-										copy: project.linkCopy,
-										url: `/projects/${project.slug}`,
-										target: '_self',
-										tag: 'p',
-										icon: 'faAngleRight',
-									}}
-								/>
-							)}
-							{project.website && (
-								<ProperLink
-									props={{
-										copy: 'Visit website',
-										url: project.website,
-										target: '_blank',
-										tag: 'p',
-										icon: 'faExternalLinkAlt',
-									}}
-								/>
-							)}
-							{project.repository && (
-								<ProperLink
-									props={{
-										copy: 'Visit repository',
-										url: project.repository,
-										target: '_blank',
-										tag: 'p',
-										icon: 'faGithub',
-									}}
-								/>
-							)}
-						</div>
+			{projects.map((project) => (
+				<div className={styles[columnOrder(project.order)]} key={project.order}>
+					<div className={styles.image}>
+						<ImageContainer props={{ image: project.thumbnail }} />
 					</div>
-				) : (
-					<div className={styles.even} key={project.order}>
-						<div>
-							<h2>{project.title}</h2>
-							<ReactMarkdown source={project.projectExcerpt} />
-							{project.showProjectLink && (
-								<ProperLink
-									props={{
-										copy: project.linkCopy,
-										url: `/projects/${project.slug}`,
-										target: '_self',
-										tag: 'p',
-										icon: 'faAngleRight',
-									}}
-								/>
-							)}
-							{project.website && (
-								<ProperLink
-									props={{
-										copy: 'Visit website',
-										url: project.website,
-										target: '_blank',
-										tag: 'p',
-										icon: 'faExternalLinkAlt',
-									}}
-								/>
-							)}
-							{project.repository && (
-								<ProperLink
-									props={{
-										copy: 'Visit repository',
-										url: project.repository,
-										target: '_blank',
-										tag: 'p',
-										icon: 'faGithub',
-									}}
-								/>
-							)}
-						</div>
-						<div className={styles.image}>
-							<ImageContainer props={{ image: project.thumbnail }} />
-						</div>
+					<div className={styles.content}>
+						<h2>{project.title}</h2>
+						<ReactMarkdown source={project.projectExcerpt} />
+						{project.showProjectLink && (
+							<ProperLink
+								props={{
+									copy: project.linkCopy,
+									url: `/projects/${project.slug}`,
+									target: '_self',
+									tag: 'p',
+									icon: 'faAngleRight',
+								}}
+							/>
+						)}
+						{project.website && (
+							<ProperLink
+								props={{
+									copy: 'Visit website',
+									url: project.website,
+									target: '_blank',
+									tag: 'p',
+									icon: 'faExternalLinkAlt',
+								}}
+							/>
+						)}
+						{project.repository && (
+							<ProperLink
+								props={{
+									copy: 'Visit repository',
+									url: project.repository,
+									target: '_blank',
+									tag: 'p',
+									icon: 'faGithub',
+								}}
+							/>
+						)}
 					</div>
-				)
-			)}
+				</div>
+			))}
 		</>
 	);
 }
