eddf370ae3677b5a495a8507269ab5bb9ebec513
dcaste <dax@daxcastellon.dev>
2021-04-28 21:00:34 +0200

    Fixed error not founding Homepage

--- pages/[[...slug]].js
+++ pages/[[...slug]].js
@@ -4,11 +4,13 @@ import { fetchAPI } from '@/utils/api';
 import renderContent from '@/utils/renderContent';
 
 const DynamicPage = ({ page: { title, seo, content } }) => {
+	const pageTitle = title === 'Homepage' ? seo.description : title;
+
 	return (
 		<Layout>
 			<Head>
 				<title>
-					{siteTitle} - {title}
+					{siteTitle} - {pageTitle}
 				</title>
 				<meta name='description' content={seo.description} />
 			</Head>
@@ -19,7 +21,6 @@ const DynamicPage = ({ page: { title, seo, content } }) => {
 
 export async function getStaticPaths() {
 	const pages = await fetchAPI('/pages');
-
 	return {
 		paths: pages.map((page) => ({
 			params: { slug: [page.slug] },
@@ -29,7 +30,10 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params, preview = null }) {
-	const pages = await fetchAPI(`/pages?slug=${params.slug}`);
+	// To get the homepage, find the only page where slug is an empty string.
+	const slug = params == {} || !params.slug ? '' : params.slug;
+
+	const pages = await fetchAPI(`/pages?slug=${slug}`);
 
 	if (pages == null) {
 		return { props: {} };
