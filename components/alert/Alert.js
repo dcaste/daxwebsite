5500f719fbf0bb794073a28ebf8b3769ceeafc5a
dcaste <dax@daxcastellon.dev>
2021-05-02 16:57:15 +0200

    Refactured Alert component

--- components/alert/Alert.js
+++ components/alert/Alert.js
@@ -3,43 +3,36 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Alert.module.scss';
 
 const Alert = ({ severity, children }) => {
-	let alertSeverity = '';
 	let alertTitle = '';
 	let icon = '';
 
 	switch (severity) {
 		case 'error':
-			alertSeverity = 'alertError';
 			alertTitle = 'Error';
 			icon = 'exclamation-triangle';
 			break;
 
 		case 'warning':
-			alertSeverity = 'alertWarning';
 			alertTitle = 'Warning';
 			icon = 'info-circle';
 			break;
 
 		case 'success':
-			alertSeverity = 'alertSuccess';
 			alertTitle = 'Success';
 			icon = 'check-circle';
 			break;
 
 		default:
-			alertSeverity = 'alertInfo';
 			alertTitle = 'Info';
 			icon = 'info-circle';
 			break;
 	}
 
 	return (
-		<div className={styles[alertSeverity]}>
-			<p>
-				<strong>
-					<FontAwesomeIcon icon={['fas', icon]} className='marginRight' />
-					{alertTitle}
-				</strong>
+		<div className={styles[severity]}>
+			<p className={styles.header}>
+				<FontAwesomeIcon icon={['fas', icon]} className='marginRight' />
+				{alertTitle}
 			</p>
 			{children}
 		</div>
@@ -47,7 +40,7 @@ const Alert = ({ severity, children }) => {
 };
 
 Alert.propTypes = {
-	severity: PropTypes.string,
+	severity: PropTypes.string.isRequired,
 };
 
 Alert.defaultProps = {
