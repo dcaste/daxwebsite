b37cfa38f5bfdfbc3d0b467f86a9cd52c6247742
dcaste <dax@daxcastellon.dev>
2021-05-02 17:36:09 +0200

    Refactored with better semantic names

--- components/gototop/GoToTop.js
+++ components/gototop/GoToTop.js
@@ -9,7 +9,7 @@ const GoToTop = ({ show }) => {
 				<motion.a
 					href='#mainWrapper'
 					id='goToTop'
-					className={styles.goToTop}
+					className={styles.button}
 					initial={{ opacity: 0, scale: 0 }}
 					animate={{ opacity: 1, scale: 1 }}
 					exit={{ opacity: 0, scale: 0 }}
