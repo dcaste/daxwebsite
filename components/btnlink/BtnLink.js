3fcb37e4fe1857f26f3a569e608cec04fbead837
dcaste <dax@daxcastellon.dev>
2021-05-24 18:57:42 +0200

    Fixed gap space in Button Group

--- components/btnlink/BtnLink.js
+++ components/btnlink/BtnLink.js
@@ -2,13 +2,13 @@ import PropTypes from 'prop-types';
 import Link from 'next/link';
 import styles from './BtnLink.module.scss';
 
-function BtnLink({ props: { copy, URL, target, style } }) {
+function BtnLink({ props: { copy, url, target, style } }) {
 	return target === '_blank' ? (
-		<a className={styles[style]} href={URL} target={target}>
+		<a className={styles[style]} href={url} target={target}>
 			{copy}
 		</a>
 	) : (
-		<Link href={URL}>
+		<Link href={url}>
 			<a className={styles[style]} target={target}>
 				{copy}
 			</a>
@@ -18,14 +18,14 @@ function BtnLink({ props: { copy, URL, target, style } }) {
 
 BtnLink.propTypes = {
 	copy: PropTypes.string,
-	URL: PropTypes.string,
+	url: PropTypes.string,
 	target: PropTypes.string,
 	style: PropTypes.string,
 };
 
 BtnLink.defaultProps = {
 	copy: 'Copy goes here',
-	URL: '#',
+	url: '#',
 	target: '_self',
 	style: 'btnMain',
 };
